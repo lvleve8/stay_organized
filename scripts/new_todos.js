0d450959e17e46281c03a8d3d3f2442bdaf0b2fc
Antonio <asoriano@appdev.yearup.org>
2024-06-14 15:03:58 -0500

    generate dropdown with correct information

--- scripts/new_todos.js
+++ scripts/new_todos.js
@@ -1,2 +1,29 @@
 "use strict"
-console.log("hi lazy team")
+
+window.onload = () => {
+    const usernameDDL = document.querySelector("#usernameDDL");
+    const categoryDDL = document.querySelector("#categoryDDL");
+
+    populate(usernameDDL, "username", "http://localhost:8083/api/users");
+    populate(categoryDDL, "name", "http://localhost:8083/api/categories");
+}
+
+async function getData(url) {
+    let response = await fetch(url, {});
+    let data = await response.json();
+
+    return data;
+}
+
+async function populate(dropdown, key, url) {
+    let dataArray = await getData(url);
+
+    dataArray.forEach((dataEntry) => {
+        let newOption = document.createElement("option");
+
+        newOption.textContent = dataEntry[`${key}`];
+        newOption.value = dataEntry.id;
+
+        dropdown.appendChild(newOption);
+    })
+}
