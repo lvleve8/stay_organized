ffa8f9c0aee6ec5a6ab4b7599f553d3f5070bef7
Antonio <asoriano@appdev.yearup.org>
2024-06-17 10:33:47 -0500

    create input fields and add POST functionality

--- scripts/new_todos.js
+++ scripts/new_todos.js
@@ -1,11 +1,14 @@
 "use strict"
 
 window.onload = () => {
+    const newTodoForm = document.querySelector("#newTodoForm");
     const usernameDDL = document.querySelector("#usernameDDL");
     const categoryDDL = document.querySelector("#categoryDDL");
 
-    populate(usernameDDL, "username", "http://localhost:8083/api/users");
-    populate(categoryDDL, "name", "http://localhost:8083/api/categories");
+    populate(usernameDDL, "username", "id", "http://localhost:8083/api/users");
+    populate(categoryDDL, "name", "name", "http://localhost:8083/api/categories");
+
+    newTodoForm.addEventListener("submit", (event) => someFn(event));
 }
 
 async function getData(url) {
@@ -15,15 +18,33 @@ async function getData(url) {
     return data;
 }
 
-async function populate(dropdown, key, url) {
+async function populate(dropdown, textContentKey, valueKey, url) {
     let dataArray = await getData(url);
 
     dataArray.forEach((dataEntry) => {
         let newOption = document.createElement("option");
 
-        newOption.textContent = dataEntry[`${key}`];
-        newOption.value = dataEntry.id;
+        newOption.textContent = dataEntry[`${textContentKey}`];
+        newOption.value = dataEntry[`${valueKey}`];
 
         dropdown.appendChild(newOption);
     })
+}
+
+async function someFn(event) {
+    event.preventDefault();
+
+    const myForm = event.target;
+
+    const formData = new FormData(myForm);
+    const formDataAsObject = Object.fromEntries(formData);
+
+    const response = await fetch("http://localhost:8083/api/todos", {
+        method: "POST",
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+        body: JSON.stringify(formDataAsObject)
+    });
+
+    let data = await response.json();
+    console.log(data);
 }
